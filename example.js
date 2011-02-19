175ec10035d6c2ab5ecc5d7b3d316baa7e47d37c
Mike Cantelon <mcantelon@gmail.com>
2011-02-18 19:20:00 -0800

    More minor cleanup.

--- example.js
+++ example.js
@@ -14,63 +14,63 @@ connect.createServer(
 
   function (request, response) {
 
-	var base_dir
-		, start_dir
-		, url_data
-		, query_data
-		, session
-		, rel_path
+	  var base_dir
+		  , start_dir
+		  , url_data
+		  , query_data
+		  , session
+		  , rel_path
 
-	// get URL query parameters
-	url_data = url.parse(request.url, true)
-	query_data = url_data.query || {}
+	  // get URL query parameters
+	  url_data = url.parse(request.url, true)
+	  query_data = url_data.query || {}
 
-	// init session variable for directory state
-	var state = request.session.state
-	state = state || {}
+	  // init session variable for directory state
+	  var state = request.session.state
+	  state = state || {}
 
-	// handle directory state change requests
-	if (query_data.toggle) {
-		rel_path = (query_data.path && query_data.path != '/')
-			? query_data.path
-			: ''
-		rel_path += query_data.toggle
-		state[rel_path] = (state[rel_path]) ? false : true
-	}
+	  // handle directory state change requests
+	  if (query_data.toggle) {
+		  rel_path = (query_data.path && query_data.path != '/')
+			  ? query_data.path
+			  : ''
+		  rel_path += query_data.toggle
+		  state[rel_path] = (state[rel_path]) ? false : true
+	  }
 
-	// set base and start directories
-	base_dir = __dirname
-	start_dir = (query_data.dir) ? base_dir + '/' + query_data.dir : base_dir
+	  // set base and start directories
+	  base_dir = __dirname
+	  start_dir = (query_data.dir) ? base_dir + '/' + query_data.dir : base_dir
 
-	// if base and start directories exist, show HTML representation of directories
-	path.exists(base_dir, function (exists) {
-		if (exists) {
-			path.exists(start_dir, function (exists) {
-				if (exists) {
-					query_data.dir = query_data.dir || ''
+	  // if base and start directories exist, show HTML representation of directories
+	  path.exists(base_dir, function (exists) {
+		  if (exists) {
+			  path.exists(start_dir, function (exists) {
+				  if (exists) {
+					  query_data.dir = query_data.dir || ''
 
-					request.session.state = state
+					  request.session.state = state
 
-					response.writeHead(200, {'Content-Type': 'text/html'})
+					  response.writeHead(200, {'Content-Type': 'text/html'})
 
-					// the tag soup below is pretty gross... prolly need to have it use file/dir templates
-					var dir = new d2h.DirToHTML(base_dir, start_dir, {
-						'parent_link_html': '<a href="/?dir={parent}">[Back]</a>&nbsp;',
-						'directory_prefix_html': '<a href="{script}?dir=' + query_data.dir + '&path={path}&toggle={entry}"><img src="/images/folder_{state}.png" border=0 /></a><img src="/images/folder_icon.png">&nbsp;<a href="/?dir={path}{entry}">',
-						'directory_suffix_html': '</a>',
-						'default_state': 'closed',
-						'initial_state': state
-					})
+					  // the tag soup below is pretty gross... prolly need to have it use file/dir templates
+					  var dir = new d2h.DirToHTML(base_dir, start_dir, {
+						  'parent_link_html': '<a href="/?dir={parent}">[Back]</a>&nbsp;',
+						  'directory_prefix_html': '<a href="{script}?dir=' + query_data.dir + '&path={path}&toggle={entry}"><img src="/images/folder_{state}.png" border=0 /></a><img src="/images/folder_icon.png">&nbsp;<a href="/?dir={path}{entry}">',
+						  'directory_suffix_html': '</a>',
+						  'default_state': 'closed',
+						  'initial_state': state
+					  })
 
-					response.write('<html><head></head><body>')
-					response.write(dir.parent_link_html() + dir.as_html())
-					response.write('</body></html>')
-					response.end()
-				}
-			})
-		}
-	})
-
-}).listen(8000)
+					  response.write('<html><head></head><body>')
+					  response.write(dir.parent_link_html() + dir.as_html())
+					  response.write('</body></html>')
+					  response.end()
+				  }
+			  })
+		  }
+	  })
+  }
+).listen(8000)
 
 sys.puts('Server running at http://127.0.0.1:8000/')
